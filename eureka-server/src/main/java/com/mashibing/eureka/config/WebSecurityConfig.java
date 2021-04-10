78cc4986839b0ab71221db82ed412a0280288d0b
zhao.song <song0586@126.com>
2021-04-11 01:08:48 +0800

    feat(eureka): security安全连接 和 高可用eureka服务端集群搭建

---
+++ eureka-server/src/main/java/com/mashibing/eureka/config/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.mashibing.eureka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/***********************
+ * @Description: spring -security 关闭跨域攻击<BR>
+ * @author: zhao.song
+ * @since: 2021/4/11 0:18
+ * @version: 1.0
+ ***********************/
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //如果服务注册报错(security默认开启了防跨域攻击)
+    //Root name 'timestamp' does not match expected ('instance') for type [simple
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //关闭跨域
+        http.csrf().disable();
+        super.configure(http);
+    }
+}
