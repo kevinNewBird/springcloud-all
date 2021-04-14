6c37bf386876b20f851d3a58aaea3c4dbaafdf15
zhao.song <song0586@126.com>
2021-04-14 18:53:14 +0800

    feat(OpenFeign): 不结合eureka, 就是自定义一个client名字. 就用url属性指定服务器列表(只需要启动user-provider和user-consumer)

---
+++ user-consumer/src/test/java/com/mashibing/eureka/UserConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashibing.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
